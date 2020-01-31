26d010f9b1c0b157fe3c0e23320e1ffa7761bd00
fnanez001 <fnanez001@student.butte.edu>
2020-01-31 03:43:01 +0000

    adding code line for hello world

---
+++ Lab1.2.1/main.go
@@ -0,0 +1,14 @@
+// Frank Nanez 
+// 01-30-2020
+// Module 1.2.1
+
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Println("Hello world!")
+	fmt.Println("My name is Frank, and my major is AS in Computer Science.")
+	fmt.Println("This is my first semester at Butte College.")
+	fmt.Println("One of my favorite pastimes is gaming.")
+}
