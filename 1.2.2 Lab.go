32f339801dce6d0e45d499bd4dd1db6bab86f5dc
fnanez001 <fnanez001@student.butte.edu>
2020-02-05 04:09:42 +0000

    added new file lab 1.2.2

---
+++ 1.2.2 Lab.go
@@ -0,0 +1,32 @@
+// Frank Nanez
+// 2-4-20
+// Lab 1.2.2 CSCI20
+
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Println("Hello World")
+  fmt.Println()
+  fmt.Println("Estimated Popluation in 10 years in the US")
+  fmt.Println("Population is;",329234331 )
+  fmt.Println("Death rate every; 10 seconds")
+  fmt.Println("Birth rate every; 9 seconds")
+  fmt.Println()
+  fmt.Println("Deaths over the next 10 years =", ((24*60*60*365*10)/10))
+  fmt.Println("Births over the next 10 years =", ((24*60*60*365*10)/9))
+  fmt.Println("Population in 10 years =", 329234331-(10*((24*60*60*365)/10))+(10*((24*60*60*365)/9)))
+
+fmt.Println()
+fmt.Println("My name is Frank!")
+fmt.Println()
+fmt.Println("FFFFF RR       A     N   N K  K")
+fmt.Println("F     R R     A A    NN  N K K")
+fmt.Println("FFF   RR     A A A   N N N KK")
+fmt.Println("F     R R   A     A  N  NN K K")
+fmt.Println("F     R  R A       A N   N K  K")
+fmt.Println()
+fmt.Println("Thank You!")
+}
+
