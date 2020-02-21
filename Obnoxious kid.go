baee1d6440cc6a8d7ec12db620b9244b8614192a
fnanez001 <fnanez001@student.butte.edu>
2020-02-21 01:17:33 +0000

    added obnoxious kid.go

---
+++ Obnoxious kid.go
@@ -0,0 +1,28 @@
+// Frank Nanez
+// 2-20-20
+// have kid ask are we there yet the number of times equal to their age
+
+package main
+
+import "fmt" 
+
+var (
+  i = 0
+  age = 0
+)
+
+func main() {
+  fmt.Println("Child: How long is this trip going to take")
+  fmt.Println("Driver: We will get there when we get there.")
+  fmt.Println("Child: Are we there yet?")
+  fmt.Println("Driver: Please don't ask again")
+  fmt.Println("Driver: I got it, how old are you?")
+
+  fmt.Scanln(&age)
+  fmt.Println("Driver: Nice! So you are",age,"!")
+
+  for i = 0; i <= age; i++{
+    fmt.Println("Kid: Are we there yes?!?!")
+}
+
+}
