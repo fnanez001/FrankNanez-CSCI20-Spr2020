c6077b8c2c501e41f00c65aad81dc286131b31a6
fnanez001 <fnanez001@student.butte.edu>
2020-03-04 04:38:23 +0000

    added baseball file

---
+++ baseball stats.go
@@ -0,0 +1,37 @@
+//
+//
+//
+
+package main
+
+import "fmt"
+
+func main() {
+  var player = [4]string {
+    "Joe",
+    "Bill",
+    "Frank",
+    "JaShen",
+    }
+
+  var battingAvg = [4]float32 {
+    1.2,
+    2.1,
+    4.5,
+    1.1,
+    }
+  
+  var sluggingAvg = [4]float32 {
+    1.1,
+    4.3,
+    5.1,
+    1.0,
+    }
+
+    fmt.Println("Here are the player stats: Name, Batting Average, Slugging Average")
+  for i := 0; i < 4; i++ {
+  fmt.Println(player[i],",", battingAvg[i],",", sluggingAvg[i])
+  }
+   
+
+}
