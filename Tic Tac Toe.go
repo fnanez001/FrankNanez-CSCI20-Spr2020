ef17d45380e4a5dcc12ab57e5e481087fff81dfa
fnanez001 <fnanez001@student.butte.edu>
2020-03-04 22:59:07 +0000

    finished Tic Tac Toe

--- Tic Tac Toe.go
+++ Tic Tac Toe.go
@@ -7,7 +7,7 @@ package main
 import "fmt"
 
 func main() {
-  var board [10]string
+  var board [9]string
   board[0]= "o"
   board[1]= "o"
   board[2]= "o"
@@ -17,12 +17,20 @@ func main() {
   board[6]= "o"
   board[7]= "o"
   board[8]= "o"
-  board[9]= "o"
   
   var userRune rune
    
-  fmt.Println( board[0],"|",board[1],"|",board[2],"\n",board[3],"|",board[4],"|",board[6],"\n",board[7],"|",board[8],"|",board[9])
+  fmt.Println( board[0],"|",board[1],"|",board[2],"\n",board[3],"|",board[4],"|",board[5],"\n",board[6],"|",board[7],"|",board[8])
   fmt.Println("Please select where you would like to enter an 'x' on the grid, starting in the top left acrosse then next line same thing and so on for the last line. i.e. top left is box 1 and top right is box 3. Then middle row left is 4.")
   fmt.Scanln(&userRune)
   
+  for i := 0; i < 10; i++ {
+   if board[userRune-1] == "o" {
+     board[userRune-1]=  "x"
+     fmt.Println( board[0],"|",board[1],"|",board[2],"\n",board[3],"|",board[4],"|",board[5],"\n",board[6],"|",board[7],"|",board[8])
+     fmt.Println("Where would you like to place an 'x' next?")
+     fmt.Scanln(&userRune)
+   }
+   
+  }
 }
