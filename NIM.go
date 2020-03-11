fa5442258d77fb5f56f4899fd5af1673f25ff7b7
fnanez001 <fnanez001@student.butte.edu>
2020-03-11 02:48:54 +0000

    commented on NIM

--- NIM.go
+++ NIM.go
@@ -26,17 +26,21 @@ fmt.Println("Pile 1:",pile1,"Pile 2:",pile2,"Pile 3:",pile3)
  var playerT int
  var pPile int
  var winner string 
-
+ // explain game to player
  fmt.Println("In this game you play against the computer, removing 1,2, or 3 stones from each pile. Winner removes the last stones.")
 
+ // set loop for piles to be greater than 0 so game loops until all piles are 0
 for pile1 > 0 || pile2 > 0 || pile3 > 0 {
+   //ask player for  amount and user input
    fmt.Println("Please enter your amount you wish to remove from the pile.")
    fmt.Scanln(&playerT)
+   //ask user for which pile and input
    fmt.Println("Please select the pile you wish to remove the stones from. Select pile 1,2, or 3.")
    fmt.Scanln(&pPile)
+   //set computer amount and pile
    cPile := (rand.Intn(2)+1)
    computerT := (rand.Intn(2)+1)
-
+   // check piles to see if game is over after user turn and all piles are 0, if 0 after player turn then player wins
    if pPile == 1 {
      pile1 = (pile1-playerT)
    }else if pPile == 2 {
@@ -44,8 +48,9 @@ for pile1 > 0 || pile2 > 0 || pile3 > 0 {
    }else if pPile == 3 {
      pile3 = (pile3-playerT)
    }
+   //print player selection to help keep track of game
   fmt.Println("User picked: ", playerT,"\n", "Pile 1:", pile1, "Pile 2:", pile2, "Pile 3:", pile3)
-
+   //check piles to see if game is over after user turn and all piles are 0, if 0 after player turn then computer wins
    if pile1 == 0 || pile2 == 0 || pile3 == 0 {
      winner = "Player"
    }else if cPile == 1 {
@@ -57,9 +62,11 @@ for pile1 > 0 || pile2 > 0 || pile3 > 0 {
    }else {
      winner = "Computer"
    }
+   //print computer selection to help keep track of game 
      fmt.Println("Computer picked: ", computerT, "Pile :",cPile)
    fmt.Println("Pile 1:",pile1,"Pile 2:",pile2,"Pile 3:",pile3)
    
   }
+  //once loop has ended then game is over, anounce the winner
   fmt.Println("Winner is",winner,"!")
 }
