5d740a65f7bc58d9a0f5d578e99283327aacd7eb
fnanez001 <fnanez001@student.butte.edu>
2020-04-03 01:11:16 +0000

    added lab 3.2.1 part 1

---
+++ 3.2.1 lab1.go
@@ -0,0 +1,19 @@
+// Programmer name: __________
+// Date completed:  __________
+// Description: ___________________________
+
+package main
+
+import "fmt"
+
+//create a function that draws ASCII art of some type
+func ascii () {
+  fmt.Println("^--^")
+  fmt.Println("(  )")
+  fmt.Println(" '' ")
+}
+
+func main() {
+    //call your ascii art function
+    ascii()
+}
