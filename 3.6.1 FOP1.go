5cb14293f62364f0882c269900fd6c1299a5cf50
fnanez001 <fnanez001@student.butte.edu>
2020-04-11 06:17:17 +0000

    completed 3.6.1 FOP1

--- 3.6.1 FOP1.go
+++ 3.6.1 FOP1.go
@@ -8,17 +8,19 @@ import "fmt"
 
 func hoursMinutesseconds(seconds int) (int, int, int) {
   hours := (seconds/60/60)
-  minutes := (hours%60)/60
-  second := (minutes%60)
+  minutes := ((seconds-(hours*60*60))/60)
+  second := (seconds-(hours*60*60)-(minutes*60))
 	return hours, minutes, second
 }
 
 func main() {
   var time int
-  var hours, minutes, seconds = hoursMinutesseconds(hours,minutes,second)
+  
 //call your function
  fmt.Println("Please give the number of seconds you wish to convert ot hours, minutes, seconds")
  fmt.Scanln(&time)
+
+ var hours, minutes, seconds = hoursMinutesseconds(time)
 
  fmt.Println("Hours:",hours,"Minutes:",minutes,"Seconds:",seconds)
 
