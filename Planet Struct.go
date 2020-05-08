2328d43c3ab89bfe13213f93e9e776fc3b13c6be
fnanez001 <fnanez001@student.butte.edu>
2020-05-08 19:17:44 +0000

    finalized planet struct

--- Planet Struct.go
+++ Planet Struct.go
@@ -4,7 +4,10 @@
 
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "strings"
+)
 
 type Planet struct{
   Name string
@@ -12,8 +15,9 @@ type Planet struct{
   Moons int
 }
 
-func weightOnplanet (gavity float64) Weight float64{
-
+func weightOnplanet(gravity float64, weight float64) (Weight float64){
+ Weight = gravity*weight 
+ return Weight
 }
 
 func main(){
@@ -25,38 +29,62 @@ func main(){
   }
   planet[1] = Planet{
     Name: "Venus",
-    Gravity: ,
-    Moons: ,
+    Gravity: 0.907,
+    Moons: 0,
   }
   planet[2] = Planet{
-    Name: "",
-    Gravity: ,
-    Moons: ,
+    Name: "Earth",
+    Gravity: 1.0,
+    Moons: 1,
   }
   planet[3] = Planet{
-    Name: "",
-    Gravity: ,
-    Moons: ,
+    Name: "Mars",
+    Gravity: 0.377,
+    Moons: 2,
   }
   planet[4] = Planet{
-    Name: "",
-    Gravity: ,
-    Moons: ,
+    Name: "Jupiter",
+    Gravity: 2.36,
+    Moons: 79,
   }
   planet[5] = Planet{
-    Name: "",
-    Gravity: ,
-    Moons: ,
+    Name: "Saturn",
+    Gravity: 0.916,
+    Moons: 82,
   }
   planet[6] = Planet{
-    Name: "",
-    Gravity: ,
-    Moons: ,
+    Name: "Uranus",
+    Gravity: 0.889,
+    Moons: 27,
   }
   planet[7] = Planet{
-    Name: "",
-    Gravity: ,
-    Moons: ,
+    Name: "Neptune",
+    Gravity: 1.12,
+    Moons: 14,
   }
+
+  var weight float64
+  for i:=0; i<8; i++{
+    fmt.Println(planet[i].Name,"gravity:",planet[i].Gravity,"number of moons:",planet[i].Moons)
+  }
+
+  var pName string
+  fmt.Println("Above are the planets, the gravity in comparison to Earth's gravity, and the number of moons for that planet. If you select a planet and enter your weight we can tell you what your wieght would be on those planets. Let's start with the name of the planet you would like to select?")
+  fmt.Scanln(&pName)
+  fmt.Println("Great! Next is your weight, in pounds, here on Earth?")
+  fmt.Scanln(&weight)
+  pName = strings.Title(pName)
+
+  x := 0
+  for i:=0; i<8; i++{
+    if pName == planet[i].Name{
+     fmt.Println("the gravity on that planet is",planet[x].Gravity,"Compared to Earth's.")
+     newWeight := weightOnplanet(planet[x].Gravity,weight)
+     fmt.Println("Your weight would be:",newWeight,"lbs.")
+    }
+    x=x+1
+  }
+  
+
 
 }
